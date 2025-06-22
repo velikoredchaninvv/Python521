a36712caecfea0d8392602255dae97268077d6e3
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-06-22 17:18:33 +0300

    Выполнил упражнение

---
+++ JavaScript/20250620 Практика/script.js
@@ -0,0 +1,28 @@
+// const three = 3;
+// let current_number = 2;
+// const finish_number = 9;
+
+// while (current_number < finish_number) {
+//     current_number++
+//     console.log(three + '*' + current_number + '=' + (three*current_number))
+// }
+
+
+// let current = 3
+// let finish = 100
+// const step = current
+
+// while (current < finish) {
+//     console.log(current)
+//     current = current + step
+// }
+
+// Напишите цикл, в котором в консоль выводятся все четные цифры
+
+current = 0
+target = 10
+
+while (current < target) {
+    console.log(current)
+    current = current + 2
+}
