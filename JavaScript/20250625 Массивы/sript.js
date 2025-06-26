ae581ac2c8bad59c168c83b408876bd2c077e8ed
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-06-26 15:39:07 +0300

    Коммиты, которые накопились после занятия в Top

---
+++ JavaScript/20250625 Массивы/sript.js
@@ -0,0 +1,12 @@
+const namee = 'Вася'
+
+const len = namee.length
+console.log(len)
+
+let iter = 0
+console.log(namee[iter])
+
+while (namee[iter] < len){
+    console.log(namee[iter])
+    iter++
+}
