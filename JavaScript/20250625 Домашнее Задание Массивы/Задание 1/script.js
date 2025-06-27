1d44566bdcf0371774dc836bdedc893f47192043
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-06-27 18:13:36 +0300

    Выполнил Задание 1 по Массивам; Остались вопросы с выводом при равенстве символа искомому символу сообщения

---
+++ JavaScript/20250625 Домашнее Задание Массивы/Задание 1/script.js
@@ -0,0 +1,35 @@
+// const imay = 'Получил каждый символ массива'
+// console.log(imay[0])
+
+// Пример 1. Вывести все буквы имени
+
+// let bukva_nomer = 0
+// while (bukva_nomer < imay.length) {
+//     console.log(imay[bukva_nomer])
+//     bukva_nomer++
+// }
+
+
+// let nomer_bukvi = 0
+// while(nomer_bukvi < imay.length){
+//     console.log(imay[nomer_bukvi])
+//     nomer_bukvi++
+// }
+
+// const myletter = 'B'
+// console.log(myletter.charCodeAt(0))
+// console.log(String.fromCharCode(97))
+
+let academy = 'TOP'
+let start = 0
+while(start<academy.length){
+    let current = academy[start]
+    console.log(academy[start])
+    console.log(current.charCodeAt(0))
+        // if (academy[start] = P){
+        //     console.log("Номер буквы P")
+        // } else {
+        //     start++
+        // }
+    start++
+}
