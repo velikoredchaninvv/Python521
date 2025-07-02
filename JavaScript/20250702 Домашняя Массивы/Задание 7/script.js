a64fb82fbc723b2680a1049ddb59443829001e75
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-07-02 20:38:57 +0300

    Сделал домашнее задание

---
+++ JavaScript/20250702 Домашняя Массивы/Задание 7/script.js
@@ -0,0 +1,9 @@
+// 7.
+// задание: дана строка let mystr = 'nice'. Вывести в консоль (циклом): первая - n, вторая - i, третья -
+// c, четвертая - e
+
+let mystr = 'nice'
+for (i=0; i < mystr.length; i++) {
+    letter = mystr[i]
+    console.log(letter)
+}
