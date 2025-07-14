423cf13f8cb547e1a3e729ecd861bae0c2105480
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-07-14 23:57:40 +0300

    Написал ещё один класс, вроде что то начал понимать

--- JavaScript/20250711 Classes/5. class man/program.js
+++ JavaScript/20250711 Classes/5. class man/program.js
@@ -6,6 +6,16 @@ class Man {
         this.lifetime = 0
     }
     time_of_life(time){
-        return this.lifetime =+ time
+        this.lifetime =+ time
     }
 }
+
+
+const nMan = new Man(39,75,'Slava')
+
+console.log(`Age of the man ${nMan.age}`)
+console.log(`Weight of the man ${nMan.weight}`)
+console.log(`Name of the man ${nMan.name}`)
+
+nMan.time_of_life(120)
+console.log(`Time of life ${nMan.lifetime}`)
