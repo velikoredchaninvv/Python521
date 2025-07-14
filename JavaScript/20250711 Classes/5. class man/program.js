2020b932f4977523332ab7c685701e168b29f444
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-07-14 23:44:15 +0300

    Добавил стуктуру класса и ввёл метод time_of_life

---
+++ JavaScript/20250711 Classes/5. class man/program.js
@@ -0,0 +1,11 @@
+class Man {
+    constructor(age, weigh, name) {
+        this.age = age
+        this.weight = weigh
+        this.name = name
+        this.lifetime = 0
+    }
+    time_of_life(time){
+        return this.lifetime =+ time
+    }
+}
