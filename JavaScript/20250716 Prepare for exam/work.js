02ca8f94558c57e2fa1db0349142fb8745c24324
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-07-16 20:13:01 +0300

    Сделал обязательную часть экзамена. Двигаюсь в сторону списков

---
+++ JavaScript/20250716 Prepare for exam/work.js
@@ -0,0 +1,14 @@
+console.log("Начинается")
+
+// div
+function tam() {
+    console.log(numinput.value)
+    return numinput.value
+}
+
+// div
+function answer() {
+    console.log(`Там ${answerlink.value}`)
+    return answerlink.value
+}
+
