c87bbf7e54ce7f0a15663f4ffbb184020a1b851a
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-09-05 19:31:59 +0300

    Отчёт за предыдущее занятие. Начало нового.

--- Python/20250903_string/task 02.py
+++ Python/20250903_string/task 02.py
@@ -1,15 +1,19 @@
-# Артфиметическое присваивание - не изменение, а замена строки
+# # Артфиметическое присваивание - не изменение, а замена строки
 
-snachala = 'Было слово '
-potom = 'получился текст'
+# snachala = 'Было слово '
+# potom = 'получился текст'
 
-sohramin = snachala
-snachala += potom
+# sohramin = snachala
+# snachala += potom
 
-# Каковы значения переменных? Что случилось с переменной 'snachala'?
+# # Каковы значения переменных? Что случилось с переменной 'snachala'?
 
-print(snachala)
-print(sohramin)
+# print(snachala)
+# print(sohramin)
 
-print(id(snachala))
-print(id(sohramin))
+# print(id(snachala))
+# print(id(sohramin))
+
+# print('\x6d\x61\x6d\x61 \x6d\x79\x6c\x61 \x72\x61\x6d\x75')
+
+print(type(input('Введи данные ')))
