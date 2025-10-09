ef7a5f2f063ba3c4581bb77fc7da02587977a7ba
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-10-09 15:56:38 +0300

    Базово разобрался с генераторами и итераторами

--- Python/20251003_iterator/04 generator.py
+++ Python/20251003_iterator/04 generator.py
@@ -1,1 +1,17 @@
 # Генератор создаётся проще через функцию yield или генераторное выражение
+
+def my_generator(max_num):
+    for i in range(max_num):
+        yield i
+
+# Использование (Это тоже итераторор!)
+gen = my_generator(3)
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+# Или генераторное выражение
+gen_expr = (i for i in range(3))
+print(next(gen_expr))
+print(next(gen_expr))
+print(next(gen_expr))
