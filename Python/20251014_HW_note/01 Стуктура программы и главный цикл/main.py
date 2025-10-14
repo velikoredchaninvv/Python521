36b12f6bbf0cf2f783239034ad48341903b5dd9c
velikoredchaninvv <velikoredchaninvv@gmail.com>
2025-10-14 23:05:32 +0300

    step to

--- Python/20251014_HW_note/01 Стуктура программы и главный цикл/main.py
+++ Python/20251014_HW_note/01 Стуктура программы и главный цикл/main.py
@@ -6,16 +6,26 @@ while True:
         with open('notes.txt', 'w', encoding='utf-8') as f:
             f.write()
             print(f"Файл 'notes.txt' создан и очищен")
-        # pass
     
     elif cmd == 'add':
         data = input('Введите текст заметки: ')
         with open('notes.txt', 'a', encoding='utf-8') as f:
             f.write(data + '\n')
+            print('Заметка добавлена.')
 
     elif cmd == 'view':
-        # показываем заметки
-        pass
+        try:
+            with open('notes.txt', 'r', encoding='utf-8') as f:
+                lines = [line.rstrip("\n") for line in f]
+            if not lines:
+                print('Файл пуст.')
+            else:
+                for i, line in enumerate(lines, 1):
+                    print(f"{i}.{line}")
+        except FileNotFoundError:
+            print('Файл не найден. Сначала создайте файл.')
+        except Exception as e:
+            print("Ошибка при чтении файла: ", e)
     
     elif cmd == 'exit':
         print('Программа завершена.')
